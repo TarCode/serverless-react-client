14439599c2f653c8e647412da3dd04e54bc1f3d2
TarCode <tarcode33@gmail.com>
2019-10-11 09:18:00 +0200

    added and configured aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "tar-notes-uploads"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://rmhdy8zfbc.execute-api.ap-south-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_9vFaFwgQK",
+      APP_CLIENT_ID: "4pkipkn8qs0cf44gofs3fh4s8",
+      IDENTITY_POOL_ID: "ap-south-1:8aa65729-b63f-4a72-8ede-0a8f296fcd30"
+    }
+  };
